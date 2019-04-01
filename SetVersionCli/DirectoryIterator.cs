2ede85a3c276c49e32b6d0c54ea56b01a70fb1c4
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2019-04-01 10:28:49 +0200

    initial commit

---
+++ SetVersionCli/DirectoryIterator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace SetVersionCli
+{
+    internal static class DirectoryIterator
+    {
+        internal static void IterateProjectFiles(string curdir, Action<string, XmlDocument, XmlNode> action)
+        {
+            IterateFiles(curdir, action);
+            IterateProjectFiles(Directory.GetDirectories(curdir), action);
+        }
+        internal static void IterateProjectFiles(string[] dirs, Action<string, XmlDocument, XmlNode> action)
+        {
+            foreach (var dir in dirs)
+            {
+                IterateFiles(dir, action);
+                IterateProjectFiles(Directory.GetDirectories(dir), action);
+            }
+        }
+
+        internal static void IterateFiles(string dir, Action<string, XmlDocument, XmlNode> action)
+        {
+            var files = Directory.GetFiles(dir, "*.csproj");
+
+            foreach (var file in files)
+            {
+                var xml = "";
+                using (var stream = new StreamReader(file))
+                {
+                    xml = stream.ReadToEnd();
+                }
+                XmlDocument document = new XmlDocument();
+                document.LoadXml(xml);
+                var project = document.FirstChild;
+
+                if (document.ChildNodes.Count == 2)
+                {
+                    project = document.ChildNodes[1];
+                }
+
+                action.Invoke(file, document, project);
+            }
+        }
+    }
+}
---
+++ SetVersionCli/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace SetVersionCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length < 1)
+            {
+                Console.WriteLine($"Invalid params");
+                return;
+            }
+
+
+            string workingDir = Environment.CurrentDirectory;
+
+            if (args.Length >= 2) {
+                workingDir = args[1];
+            }
+
+            SetVersion(Version.Parse(args[0]), workingDir);
+        }
+
+
+        internal static int SetVersion(Version version, string workingDirectory)
+        {
+            Console.WriteLine($"Set assembly version to {version} in {workingDirectory}");
+            DirectoryIterator.IterateProjectFiles(workingDirectory, (file, document, project) =>
+            {
+                var props = project.FirstChild;
+
+                var versionProp = props.SelectNodes("Version");
+                var packageVerson = props.SelectNodes("PackageVersion");
+
+                if (packageVerson.Count == 1)
+                {
+                    packageVerson[0].InnerText = version.ToString();
+                }
+
+                if (versionProp.Count == 1)
+                {
+                    versionProp[0].InnerText = version.ToString();
+                }
+                else
+                {
+                    var node = document.CreateNode(XmlNodeType.Element, "Version", null);
+                    node.InnerText = version.ToString();
+
+                    props.AppendChild(node);
+                }
+                using (var w = new StreamWriter(file))
+                    document.Save(w);
+            });
+            return 0;
+        }
+    }
+}
